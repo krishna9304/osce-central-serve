c7685b2a546e35043ec00e3f7a4bbc1c8b805a86
Krishna Mahato <krishhtrishh9304@gmail.com>
2023-10-24 12:39:42 +0530

    ADD: app config initialized

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+  app.useWebSocketAdapter(new IoAdapter(app));
+
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('osce.ai API')
+    .setDescription(
+      'This is the API docs for osce.ai. Feel free to use it to test the API endpoints.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        description: `Please enter token in following format: Bearer <JWT>`,
+        name: 'Authorization',
+        bearerFormat: 'Bearer',
+        scheme: 'Bearer',
+        type: 'http',
+        in: 'Header',
+      },
+      'Authorization',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
