96d60d0689e3203cea1b47dda0e9eb4c0cc0043d
Krishna Mahato <krishhtrishh9304@gmail.com>
2023-10-24 22:11:45 +0530

    ADD: api response constant and server stats route

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ApiResponseType, ApiResponse } from './constants/apiResponse';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getServerHealth(): ApiResponseType {
+    const response = new ApiResponse(
+      'Server running | ' + new Date().toISOString(),
+      null,
+      200,
+    );
+    return response.getResponse();
   }
 }
