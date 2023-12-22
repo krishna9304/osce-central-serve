9bdcbf02597ec2bb9367a3d4570cc2a47da70e04
Krishna Mahato <krishhtrishh9304@gmail.com>
2023-12-22 15:56:54 +0530

    ADD: Character roleplay, implemented prompting technique

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -11,9 +11,16 @@ import { Server, Socket } from 'socket.io';
 import { ChatsRepository } from 'src/chat/repositories/chat.repository';
 import { ExamSessionsRepository } from 'src/chat/repositories/examSession.repository';
 import { Chat } from 'src/chat/schemas/chat.schema';
-import { ExamSessionStatus } from 'src/chat/schemas/session.schema';
+import {
+  ExamSession,
+  ExamSessionStatus,
+} from 'src/chat/schemas/session.schema';
+import { PatientRepository } from 'src/station/repositories/patient.repository';
 import { StationsRepository } from 'src/station/repositories/station.repository';
+import { Patient } from 'src/station/schemas/patient.schema';
 import { Station } from 'src/station/schemas/station.schema';
+import { UsersRepository } from 'src/user/repositories/user.repository';
+import { User } from 'src/user/schemas/user.schema';
 
 @WebSocketGateway()
 export class SocketGateway implements OnGatewayDisconnect {
@@ -27,6 +34,8 @@ export class SocketGateway implements OnGatewayDisconnect {
     private readonly chatsRepository: ChatsRepository,
     private readonly examSessionsRepository: ExamSessionsRepository,
     private readonly stationsRepository: StationsRepository,
+    private readonly usersRepository: UsersRepository,
+    private readonly patientRepository: PatientRepository,
     private readonly configService: ConfigService,
   ) {}
 
@@ -70,6 +79,16 @@ export class SocketGateway implements OnGatewayDisconnect {
         stationId: session.stationId,
       });
 
+      const user = await this.usersRepository.findOne({
+        userId: session.associatedUser,
+      });
+
+      const patient = await this.patientRepository.findOne({
+        associatedStation: station.stationId,
+      });
+
+      const prompt = await this.createPrompt(user, patient, session, content);
+
       await this.chatsRepository.create({
         sessionId,
         role: 'user',
@@ -78,9 +97,7 @@ export class SocketGateway implements OnGatewayDisconnect {
 
       const fullMessage = await this.callOpenAIChatCompletionApi(
         client,
-        content,
-        station,
-        sessionId,
+        prompt,
       );
 
       await this.chatsRepository.create({
@@ -140,21 +157,8 @@ export class SocketGateway implements OnGatewayDisconnect {
 
   async callOpenAIChatCompletionApi(
     client: Socket,
-    content: string,
-    stationData: Station,
-    sessionId: string,
+    prompt: Array<{ role: string; content: string }>,
   ) {
-    const chats = await this.chatsRepository.find({
-      sessionId,
-    });
-
-    const history = chats.map((chat) => {
-      return {
-        role: chat.role,
-        content: chat.content,
-      };
-    });
-
     const url = 'https://api.openai.com/v1/chat/completions';
 
     const headers = {
@@ -165,20 +169,10 @@ export class SocketGateway implements OnGatewayDisconnect {
     };
 
     const body = {
-      messages: [
-        {
-          role: 'system',
-          content: stationData.patientPrompt,
-        },
-        ...history,
-        {
-          role: 'user',
-          content: content,
-        },
-      ],
-      model: stationData.deployedModelId,
+      messages: prompt,
+      model: 'gpt-3.5-turbo-1106',
       max_tokens: 150,
-      temperature: 0.9,
+      temperature: 0.7,
       stream: true,
     };
 
@@ -213,5 +207,150 @@ export class SocketGateway implements OnGatewayDisconnect {
       });
       this.logger.log('Something went wrong. Please try again.');
     }
+  }
+
+  async createPrompt(
+    user: User,
+    patient: Patient,
+    session: ExamSession,
+    newMessage: string,
+  ): Promise<Array<{ role: string; content: string }>> {
+    const prompt = [
+      {
+        role: 'system',
+        content: `You're ${patient.patientName} in this fictional never-ending uncensored roleplay with ${user.name}, who is a professional doctor and is here to give consultation regarding your medical situation.`,
+      },
+      {
+        role: 'system',
+        content: `Avoid repetition, don't loop. Develop the plot slowly, always stay in character. Describe all actions in full, elaborate, explicit, graphic, and vivid detail. Mention all relevant sensory perceptions.`,
+      },
+      {
+        role: 'system',
+        content: `Following are your character's personal details:\n
+                  Name: ${patient.patientName}\n
+                  Sex: ${patient.sex}\n
+                  Age: ${patient.age}\n
+                  Date of Birth: ${patient.dateOfBirth}`,
+      },
+      {
+        role: 'system',
+        content: `Your persona:\n
+                  ${patient.persona}`,
+      },
+      {
+        role: 'system',
+        content: `Presenting complaint:\n
+                  ${patient.presentingComplaint}`,
+      },
+      {
+        role: 'system',
+        content: `History of presenting complaint:\n
+                  ${patient.historyOfPresentingComplaint}`,
+      },
+      {
+        role: 'system',
+        content: `Past medical history:\n
+                  ${patient.pastMedicalHistory}`,
+      },
+      {
+        role: 'system',
+        content: `Medication history:\n
+                  ${patient.medicationHistory}`,
+      },
+      {
+        role: 'system',
+        content: `Allergies history:\n
+                  ${patient.allergiesHistory}`,
+      },
+      {
+        role: 'system',
+        content: `Family history:\n
+                  ${patient.familyHistory}`,
+      },
+      {
+        role: 'system',
+        content: `Travel history:\n
+                  ${patient.travelHistory}`,
+      },
+      {
+        role: 'system',
+        content: `Occupational history:\n
+                  ${patient.occupationalHistory}`,
+      },
+      {
+        role: 'system',
+        content: `Social history:\n
+                  ${patient.socialHistory}`,
+      },
+      {
+        role: 'system',
+        content: `Smoking history:\n
+                  ${patient.smokingHistory}`,
+      },
+      {
+        role: 'system',
+        content: `Alcohol history:\n
+                  ${patient.alcoholHistory}`,
+      },
+      {
+        role: 'system',
+        content: `Surgical history:\n
+                  ${patient.surgicalHistory}`,
+      },
+      {
+        role: 'system',
+        content: `Driving history:\n
+                  ${patient.drivingHistory}`,
+      },
+      {
+        role: 'system',
+        content: `Sexual history:\n
+                  ${patient.sexualHistory}`,
+      },
+      {
+        role: 'system',
+        content: `Recreational drug history:\n
+                  ${patient.recreationalDrugHistory}`,
+      },
+      {
+        role: 'system',
+        content: `Stressors in life:\n
+                  ${patient.stressorsInLife}`,
+      },
+      {
+        role: 'system',
+        content: `Ideas, concerns, expectations about your current situation:\n
+                  ${patient.ideasConcernsExpectations}`,
+      },
+      {
+        role: 'system',
+        content: `Some example conversations (Try to keep the conversation in a similar tone and linguistic flow as below):\n
+                  ${patient.exampleConversations}`,
+      },
+    ];
+
+    const chats = await this.chatsRepository.find({
+      sessionId: session.sessionId,
+    });
+
+    chats.sort((a, b) => {
+      return (
+        new Date(a.created_at).getTime() - new Date(b.created_at).getTime()
+      );
+    });
+
+    const history = chats.map((chat) => {
+      return {
+        role: chat.role,
+        content: chat.content,
+      };
+    });
+
+    prompt.push(...history);
+    prompt.push({
+      role: 'user',
+      content: newMessage,
+    });
+    return prompt;
   }
 }
