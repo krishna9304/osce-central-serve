8666b312a8f59e505ab95ef366c8ebafcf3f6432
Krishna Mahato <krishhtrishh9304@gmail.com>
2024-02-23 01:43:56 +0530

    FEAT: openai response pipe to 11 labs request, INTEGRATION COMPLETE
    FIX: some minor bug fixes

---
+++ src/utils/elevenlabs.util.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import axios, { AxiosResponse } from 'axios';
+
+@Injectable()
+export class ElevenLabsUtil {
+  constructor(private readonly configService: ConfigService) {}
+
+  url = (voiceId) =>
+    `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}/stream`;
+  headers = {
+    accept: 'application/json',
+    'xi-api-key': this.configService.get<string>('ELEVEN_LABS_API_KEY'),
+    'Content-Type': 'application/json',
+  };
+  msgObject = (text: string = '') => ({
+    model_id: 'eleven_multilingual_v2',
+    text,
+    voice_settings: {
+      stability: 0.5,
+      similarity_boost: 0.3,
+    },
+  });
+
+  readonly getAudioStream = async (
+    text: string,
+    voiceId: string,
+  ): Promise<AxiosResponse> => {
+    try {
+      const response = await axios.post(
+        this.url(voiceId),
+        JSON.stringify(this.msgObject(text)),
+        {
+          headers: this.headers,
+          responseType: 'stream',
+        },
+      );
+      return response;
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
+  };
+}
